ffcc1b503623fd63d62a0e5adcfc558ed14208f7
DeveloperKenny <81117537+DeveloperKenny@users.noreply.github.com>
2022-11-14 17:23:58 -0800

    Changed and create new function to handle user input
    Changed and created a new function to handle user input.
    The original function created multiple rewrites(too expensive).

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,19 @@
 import React from "react";
 
+import userpic from '../Components/maleuser.png'
+
+
+
+
 
 
 export const Navbar = () => {
 
 
-    return (
+    return(
             <div className="container">
-                <div className="nav-user">
-                    User
-                </div>
+              
+                <div className="user-name"><img className="user-pic" src={userpic} /> Kennedy Lee</div>
             </div>
     )
 }
